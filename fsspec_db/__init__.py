29ca867417e6730b300a9481cb44b912fd5e92e2
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-07-12 18:54:14 -0400

    Bump version: 0.2.0 → 0.3.0
    Signed-off-by: Tim Paine <3105306+timkpaine@users.noreply.github.com>

--- fsspec_db/__init__.py
+++ fsspec_db/__init__.py
@@ -16,7 +16,7 @@ from .spec import (
 from .sqlalchemy import SQLAlchemyDatabase, SQLAlchemyDatabaseFileSystem
 from .sqlite import SQLiteDatabaseFileSystem
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = [
     "AbstractDatabase",
